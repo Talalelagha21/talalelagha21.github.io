de214a7d716893cb1b2b35e2bcfbebef54462d01
Talalelagha21 <62902443+Talalelagha21@users.noreply.github.com>
2020-03-30 20:41:01 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ original();
 
 document.querySelector('.btn').addEventListener('click', function () {
     document.querySelector(".header").textContent = "NO, please stay home";
+    document.querySelector(".header").style.color = "red";
+    document.querySelector('.help').textContent= "For more Information ";
+    document.querySelector('.link').textContent= "click here ";
+   
     
     
     
@@ -22,4 +26,7 @@ document.querySelector('.btn2').addEventListener('click', function () {
 
 function original(){
     document.querySelector('.header').textContent= "Can I leave my house? ";
+    document.querySelector('.header').style.color = "black";
+    
 }
+
